77b87c64776eb28866bd1e0e161b9f60691e4e75
Alejandro Queiruga <afqueiruga@lbl.gov>
2017-06-05 16:15:04 -0700

    Init and some documentation

---
+++ Readme.md
@@ -0,0 +1,4 @@
+afq_fenicsutil
+==============
+
+A little collections of utilities for FEniCS. Portions taken from cornflakes, an unreleased runtime by afq, and various codes by B.E. Abali. 
---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+
+This is a little library of helper functions for use with FEniCS 
+by Alejandro F Queiruga, with major contributions by B. Emek Abali.
+
+"""
+
+
+from various import *
+from my_restriction_map import restriction_map
+from write_vtk import write_vtk_f
